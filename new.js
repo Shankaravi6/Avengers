39b30baf37cf2f15ec66385e43f8746d7e288231
Shankar <69907363+Shankaravi6@users.noreply.github.com>
2021-03-28 12:05:05 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,40 @@
+
+var modal = document.getElementById('myModal');
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
+
+var images = document.getElementsByTagName('img');
+var modalImg = document.getElementById("img01");
+
+var i;
+for (i = 0; i < images.length; i++) {
+   images[i].onclick = function(){
+       modal.style.display = "block";
+       modalImg.src = this.src;
+       modalImg.alt = this.alt;
+       captionText.innerHTML = this.nextElementSibling.innerHTML;
+   }
+}
+
+// Picture Slideshow
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000);    
+}
